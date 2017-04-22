10f634e342408319257e10271dcd82d966df5181
gfnation <gfyankeesfan@aol.com>
2017-04-22 10:24:09 -0400

    Finished movies

--- main.c
+++ main.c
@@ -13,6 +13,8 @@
 #define TEXT ".txt"
 #define JPEG ".jpg"
 #define PNG ".png"
+#define CENT1 "19"
+#define CENT2 "20"
 
 char* findExtension(char* fileName);
 void organize(DIR* dir);
@@ -126,6 +128,91 @@ void organize(DIR *dir)
 }
 void vidOrg(char* fileName)
 {
+  //Using file Cent to store the century the movie was made if that's the case
+  char* fileCent = malloc(sizeof(char)*3);
+  int i =0;
+  for(; i<2;i++)
+  {
+    fileCent[i] = fileName[i];
+  }
+  fileCent[2] = '\0';
+
+
+
+  //open the old file
+  int oldFile;
+  if(oldFile = open(fileName, O_RDONLY) == -1)
+  {
+    fprintf(stderr, "%s\n", "The old movie file couldn't be opened.");
+    exit(1);
+  }
+
+  //Get the stats for the file
+  struct stat statbuf;
+  if(stat(fileName, &statbuf) == -1)
+  {
+    fprintf(stderr, "%s\n","Couldn't get stat on file" );
+    exit(1);
+  }
+  //will be used later to write old data to the new file
+  int buffer_size = statbuf.st_size;
+  char* buffer[buffer_size];
+
+
+
+  //If the file starts with 19 or 20 assume that it is a movie
+  if(strcmp(fileCent, CENT2)==0 || strcmp(fileCent, CENT1))
+  {
+    //get the movie year and movie name
+    char* year = getTokened(fileName, '-');
+    char* movieName = getRemaining(fileName, year);
+
+    //Move into movie directory
+    chdir("./movies"); DIR* dir = opendir(".");
+
+    //Turn movie year into a folder
+    char* yearDir = malloc((sizeof(char) * strlen(year))+ 2);
+    strcpy(yearDir, "./"); strcat(yearDir, year);
+
+    //Test to see if that year folder existed
+    struct dirent *yr_dir;
+    int found =1;
+    while(((yr_dir = readdir(dir)) != NULL) && found ==1)
+    {
+      if(strcmp(yr_dir->d_name, year) == 0)
+      {
+        cddir(yearDir);
+        found =0;
+      }
+    }
+    //If found = 1 then the directory isn't found
+    if(found == 1)
+    {
+      mkdir(yearDir);
+      chdir(yearDir);
+    }
+    //Now we are in the year Directory place the new file in the folder.
+    int newFile;
+    if((newFile = open(movieName, O_CREAT | O_WRONLY, 0644) == -1)
+    {
+      fprintf(stderr, "%s\n", "The new movie file couldn't be created." );
+      exit(1);
+    }
+    int n_read, o_read;
+    while ((o_read = read(oldFile, buffer, buffer_size)) > 0)
+    {
+      n_read = write(newFile, buffer, buffer_size);
+    }
+
+    close(newFile); close(oldFile);
+    chdir("../..");
+  }
+  //if not got to tv shows
+  else
+  {
+
+  }
+  remove(fileName);
 
 }
 
@@ -133,11 +220,7 @@ void musicOrg(char* fileName)
 {
 
 
-
-  char* throwAway = malloc(sizeof(char) * strlen(fileName) +1);
-  strcpy(throwAway, fileName);
-
-//Make own tokenizer
+  //Make own tokenizer
 
   char* artist = getTokened(fileName, '-');
   char* artRemaining = getRemaining(fileName, artist);
@@ -163,14 +246,11 @@ void musicOrg(char* fileName)
   char* buffer[buffer_size];
   //We know the extension will put us in music folders
   chdir("./music"); DIR *dir = opendir(".");
-////
-////
-///
+
   //Time to read the directory for the Artist name
   char* artPath = malloc((sizeof(char) * strlen(artist)) +2);
   strcpy(artPath, "./");
   strcat(artPath, artist);
-  printf("%s\n", artPath );
   struct dirent *dirent_pt;
   int found = 1;
   while(((dirent_pt = readdir(dir)) != NULL) && found ==1)
@@ -187,7 +267,6 @@ void musicOrg(char* fileName)
     mkdir(artPath, 0777);
     chdir(artPath);
   }
-  printf("%s\n", "made it" );
 
   //Check for album
   struct dirent *alb_pt;
@@ -247,73 +326,73 @@ void  easyOrg(char *fileName, char* fileExt)
     exit(1);
   }
 
-    if(strcmp(fileExt, PNG) ==0 || strcmp(fileExt, JPEG) ==0)
-    {
-      chdir("./pictures");
-      printf("%s\n","Working in Pictures now");
-    }
-    else if (strcmp(fileExt, DOC) ==0 || strcmp(fileExt, TEXT) ==0)
-    {
-      chdir("./documents");
-
-      printf("%s\n","Working in Documents now");
-    }
-    else
-    {
-
-      printf("%s\n","Working in other now");
-      chdir("./other");
-    }
-
-    int buf_size = statbuf.st_size;
-    char buffer[buf_size];
-
-    printf("%s\n", fileName);
-
-    if((nfile = open(fileName, O_CREAT | O_WRONLY, 0644)) ==-1)
-    {
-      fprintf(stderr, "%s\n", "Couldn't open the new file");
-      exit(1);
-    }
-
-
-    while ((nread = read(ofile, buffer, buf_size)) > 0)
-    {
-      nwrite = write(nfile, buffer, buf_size);
-    }
-
-    close(nfile); close(ofile);
-
-    chdir("..");
-
-    remove(fileName);
-
-  }
-
-
-
-  int ignore(char* name)
+  if(strcmp(fileExt, PNG) ==0 || strcmp(fileExt, JPEG) ==0)
   {
-    //ignore current and past directories
-    if(strcmp(name, ".") == 0 || strcmp(name, "..") ==0 || strcmp(name, "core") == 0)
-    {
-      return 1;
-    }
-    //ignore folders I made
-    else if(strcmp(name, "music") == 0 || strcmp(name, "movies") == 0 || strcmp(name, "shows") == 0 || strcmp(name, "documents")==0 || strcmp(name, "other")==0 || strcmp(name, "pictures") ==0)
-    {
-      return 1;
-    }
-    //ignore headers and c files and executables
-    else if(strcmp(findExtension(name), ".c") ==0 || strcmp(findExtension(name), ".h") ==0 || strcmp(name, "organizer") ==0)
-    {
-      return 1;
-    }
-    else
-    {
-      return 0;
-    }
+    chdir("./pictures");
+    printf("%s\n","Working in Pictures now");
   }
+  else if (strcmp(fileExt, DOC) ==0 || strcmp(fileExt, TEXT) ==0)
+  {
+    chdir("./documents");
+
+    printf("%s\n","Working in Documents now");
+  }
+  else
+  {
+
+    printf("%s\n","Working in other now");
+    chdir("./other");
+  }
+
+  int buf_size = statbuf.st_size;
+  char buffer[buf_size];
+
+  printf("%s\n", fileName);
+
+  if((nfile = open(fileName, O_CREAT | O_WRONLY, 0644)) ==-1)
+  {
+    fprintf(stderr, "%s\n", "Couldn't open the new file");
+    exit(1);
+  }
+
+
+  while ((nread = read(ofile, buffer, buf_size)) > 0)
+  {
+    nwrite = write(nfile, buffer, buf_size);
+  }
+
+  close(nfile); close(ofile);
+
+  chdir("..");
+
+  remove(fileName);
+
+}
+
+
+
+int ignore(char* name)
+{
+  //ignore current and past directories
+  if(strcmp(name, ".") == 0 || strcmp(name, "..") ==0 || strcmp(name, "core") == 0)
+  {
+    return 1;
+  }
+  //ignore folders I made
+  else if(strcmp(name, "music") == 0 || strcmp(name, "movies") == 0 || strcmp(name, "shows") == 0 || strcmp(name, "documents")==0 || strcmp(name, "other")==0 || strcmp(name, "pictures") ==0)
+  {
+    return 1;
+  }
+  //ignore headers and c files and executables
+  else if(strcmp(findExtension(name), ".c") ==0 || strcmp(findExtension(name), ".h") ==0 || strcmp(name, "organizer") ==0)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
 
 char* getTokened(char* original, char token)
 {
